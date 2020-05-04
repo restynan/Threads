f9451c75de347a9217fc9ae57ff0db5bfb02bb15
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-05-04 15:32:53 -0500

    lambda  implementation

--- lambdaExpression/src/com/example/Main.java
+++ lambdaExpression/src/com/example/Main.java
@@ -4,27 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Runnable obj1 =()->{
 
-            for (int i = 0; i < 5; i++) {
-                System.out.println("Hi");
-               try { Thread.sleep(500); } catch (Exception e) { }
-            }
-
-        };
-        Runnable obj2 =()->{
+        Thread t1 = new Thread(()->{
 
             for (int i = 0; i < 5; i++) {
                 System.out.println("Hello");
                 try { Thread.sleep(500); } catch (Exception e) { }
             }
 
-        };
+        }
+);
+        Thread t2 = new Thread(()->{
 
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hi");
+                try { Thread.sleep(500); } catch (Exception e) { }
+            }
 
-
-        Thread t1 = new Thread(obj1);
-        Thread t2 = new Thread(obj2);
+        }
+);
         t1.start();
         try { Thread.sleep(10); } catch (Exception e) { }
         t2.start();
--- threads/src/com/example/Main.java
+++ threads/src/com/example/Main.java
@@ -6,24 +6,26 @@ public class Main {
         Hi obj1 = new Hi();
         Hello obj2 = new Hello();
 
-        obj1.show();
-        obj2.show();
+        obj1.start();
+        obj2.start();
     }
 }
 
-class Hi{
-    public void show(){
+class Hi extends Thread{
+    public void run(){
         for (int i=0;i<5;i++) {
             System.out.println("Hi");
+           try{ Thread.sleep(500);}catch (Exception e){}
         }
     }
 
 }
 
-class Hello{
-    public void show(){
+class Hello extends Thread{
+    public void run(){
         for (int i=0;i<5;i++) {
             System.out.println("Hello");
+            try{ Thread.sleep(500);}catch (Exception e){}
         }
     }
 
