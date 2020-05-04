cc138a2f56ded622209e72a4043ad1072e0063e8
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-05-04 15:27:52 -0500

    using Runnable interface

---
+++ lambdaExpression/src/com/example/Main.java
@@ -0,0 +1,39 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Runnable obj1 =()->{
+
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hi");
+               try { Thread.sleep(500); } catch (Exception e) { }
+            }
+
+        };
+        Runnable obj2 =()->{
+
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hello");
+                try { Thread.sleep(500); } catch (Exception e) { }
+            }
+
+        };
+
+
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        t1.start();
+        try { Thread.sleep(10); } catch (Exception e) { }
+        t2.start();
+
+
+
+
+
+
+}
+
+}
